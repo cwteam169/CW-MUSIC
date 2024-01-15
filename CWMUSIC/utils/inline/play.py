08d0257446a66700307d82d354795b3ce12819bb
cwteam169 <156594586+cwteam169@users.noreply.github.com>
2024-01-16 03:35:40 +0530

    𝐂𝐖-𝐌𝐔𝐒𝐈𝐂

--- CWMUSIC/utils/inline/play.py
+++ CWMUSIC/utils/inline/play.py
@@ -69,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/CW_TEAM_OWNER",
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/CW_TEAM_OWNER",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/ll_CRUSH_WORLD_ll",
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/ll_CRUSH_WORLD_ll",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -91,10 +91,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/CW_TEAM_OWNER",
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/CW_TEAM_OWNER",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/ll_CRUSH_WORLD_ll",
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/ll_CRUSH_WORLD_ll",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
